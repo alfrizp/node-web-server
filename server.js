6775721a1de51867448d9f53f31e16186320de43
alip <alifrizkipambudi@gmail.com>
2017-10-29 21:31:58 +0700

    Create middleware to log to server.log

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 
@@ -9,7 +10,14 @@ app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
     var now = new Date().toString();
-    console.log(`${now}: ${req.method} ${req.url}`);
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
     next();
 });
 
